4e319ec85579eec06440d13f81e09cf62ce0f705
wkaczurba <witold.kaczurba@gmail.com>
2017-02-11 08:05:40 +0000

    Maven structure + empty test.

---
+++ SpringRestConsume1/src/test/java/com/RestTemplateTest.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration()
+public class RestTemplateTest {
+	
+	@Test
+    public void restTest1()
+    {
+		
+    }
+
+}
